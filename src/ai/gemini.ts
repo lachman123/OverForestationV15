97ce9a4c53ce86a80486999c42c0d9566fb48ea1
Gwyllim Jahn <gwyllo@gmail.com>
2024-05-07 22:41:15 +1000

    added gemini pdf example

--- src/ai/gemini.ts
+++ src/ai/gemini.ts
@@ -1,5 +1,39 @@
 "use server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GenerateContentRequest, VertexAI } from "@google-cloud/vertexai";
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const project_id = process.env.GEMINI_PROJECT_ID ?? "";
+
+export async function getGeminiPDF(file_url: string, prompt: string) {
+  const vertexAI = new VertexAI({
+    project: project_id,
+    location: "us-central1",
+  });
+
+  const generativeModel = vertexAI.getGenerativeModel({
+    model: "gemini-1.5-pro-preview-0409",
+  });
+
+  const filePart = {
+    file_data: {
+      file_uri: file_url,
+      mime_type: "application/pdf",
+    },
+  };
+  const textPart = {
+    text: prompt,
+  };
+
+  const request = {
+    contents: [{ role: "user", parts: [filePart, textPart] }],
+  } as GenerateContentRequest;
+
+  const resp = await generativeModel.generateContent(request);
+  const contentResponse = await resp.response;
+  console.log(JSON.stringify(contentResponse));
+  return contentResponse;
+}
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINI ?? "");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-wrap">
         <Link href="/timelapse">Timelapse Demo </Link>
         <Link href="/artcritic">Art Critic Demo </Link>
         <Link href="/map">Simple Map Demo </Link>
         <Link href="/exploration">Exploration</Link>
         <Link href="/experts">Experts Demo </Link>
         <Link href="/whatsthis">What is This?</Link>
+        <Link href="/realtime">Realtime image generation</Link>
       </div>
     </main>
   );
