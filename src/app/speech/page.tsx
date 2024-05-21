9b0dcb8c318a02fcdbe7bedac0ac7e7c551a9875
Gwyllim Jahn <gwyllo@gmail.com>
2024-05-21 11:35:42 +1000

    Added handleTranscription example

--- src/app/speech/page.tsx
+++ src/app/speech/page.tsx
@@ -1,10 +1,13 @@
 import SpeechToText from "@/components/SpeechToText";
 
 export default function SpeechPage() {
+  const handleTranscription = (transcription: string) => {
+    //do whatever you want here
+  };
   return (
     <main className="flex min-h-screen flex-col items-end justify-between p-24">
       <div className="z-10 max-w-lg w-full items-center justify-between font-mono text-sm lg:flex">
-        <SpeechToText />
+        <SpeechToText onTranscribed={handleTranscription} />
       </div>
     </main>
   );
