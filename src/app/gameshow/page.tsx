2c0d650e9719bd1e720da1f3a84be0fa1c41b6a4
Gwyllim Jahn <gwyllo@gmail.com>
2024-05-23 18:15:13 +1000

    added join link

--- src/app/gameshow/page.tsx
+++ src/app/gameshow/page.tsx
@@ -7,9 +7,9 @@ export default function GameshowPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-4">
           <Link href="/gameshow/host">Host Game</Link>
-          <Link href="/gameshow/host">Join Game</Link>
+          <Link href="/gameshow/join">Join Game</Link>
         </div>
       </div>
     </main>
