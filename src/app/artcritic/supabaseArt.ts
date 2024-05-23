f954079614d4fc10ce97451c01a736e12340a29b
Gwyllim Jahn <gwyllo@gmail.com>
2024-05-23 18:10:47 +1000

    fix import

--- src/app/artcritic/supabaseArt.ts
+++ src/app/artcritic/supabaseArt.ts
@@ -1,5 +1,5 @@
 "use server";
-import { supabase } from "@/supabase/supabase";
+import supabase from "@/supabase/supabase";
 
 //These functions are specific to my supabase project and save the images generated in the art critic example to a table called Map.
 //If you want to work with supabase, you will likely need some help getting your database set up on discord.
