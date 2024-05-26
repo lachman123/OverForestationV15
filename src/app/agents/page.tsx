3ccc07f645223f9586c78bd51465a70fe662da48
Gwyllim Jahn <gwyllo@gmail.com>
2024-05-26 21:39:18 +1000

    remove streetview

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -11,7 +11,7 @@ export const dynamic = "force-dynamic";
 //This is new - just provide a high level goal and groq will figure out how to make agents
 const agentGoal =
   "Build an offshore aquaculture farm to supply the worlds protein demands by 2050";
-//set your agents here. If you leave this empty then Groq creates some for you based on your graph.
+//set your agents here. If you leave this empty then Groq creates some for you based on your graph and the goal above.
 const initAgents: any = [];
 //if this is true, agents add nodes to the graph as well as update implementation data. Its slower.
 const addNodes = false;
