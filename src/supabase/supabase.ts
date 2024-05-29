2195e3ec27552f2eb961925e2b4b28e05aafb6fe
WorldEngine01 <julianpinneri@hotmail.com>
2024-05-29 18:37:18 +1000

    superbase delete
    .

--- src/supabase/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Initialize the Supabase client
-const supabaseUrl = process.env.SUPABASE_URL ?? "";
-const supabaseKey = process.env.SUPABASE_KEY ?? "";
-
-export default createClient(supabaseUrl, supabaseKey);
--- src/supabase/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-import { createClient } from "@supabase/supabase-js";
-
-// Initialize the Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "";
-
-export default createClient(supabaseUrl, supabaseKey);
